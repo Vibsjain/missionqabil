88972ecc2bdc282adcef1a963430a5d36e223767
Vibsjain <290901vibsjain@gmail.com>
2021-01-16 21:16:43 +0530

    added logo

--- src/topContainer/Navbar.js
+++ src/topContainer/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css"
+import logo from "../images/logo.png";
 
 // const currentTab = (history, path) => {
 //     if (history.location.pathname === path) {
@@ -13,6 +14,7 @@ import "./Navbar.css"
 const navbar = ({history}) => (
         <nav id='custom-navbar' className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
+                <img src = {logo} className="nav-logo" />
                 <a className="navbar-brand" href="#">Mission Qabil</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
